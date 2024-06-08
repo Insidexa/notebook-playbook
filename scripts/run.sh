4bc002978eeee054ecb6b4d7e34b7949a0c56575
Andrii Yakovenko <i.am.yashka@gmail.com>
2024-06-09 02:32:23 +0300

    use volumes intead of copy ( compatibility with dev exp )

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,7 @@
 NETWORK_NAME="ansible_bridge"
 PROJECT_DIR=$(pwd)
 USER_CNT_NAME="ansible"
+HOME_DIR="/home/$USER_CNT_NAME"
 
 # Check if the network exists
 if ! podman network inspect $NETWORK_NAME &>/dev/null; then
@@ -16,6 +17,8 @@ podman build -t playbook_ansible --build-arg USER=$USER_CNT_NAME -f docker/Docke
 podman run \
   --detach \
   --name=playground \
+  -v ./docker/config/sshd_config:/etc/ssh/sshd_config:Z,ro \
+  -v ./docker/keys/ansible.pub:$HOME_DIR/.ssh/authorized_keys:Z,ro \
   --net $NETWORK_NAME \
   --network-alias playground \
   -u root \
@@ -26,6 +29,11 @@ podman run \
   --name=ansible \
   --userns=keep-id \
   --requires=playground \
+  -v ./docker/config/ansible.cfg:/etc/ansible/ansible.cfg:ro \
+  -v ./playbook/:$HOME_DIR/playbook/:Z,ro \
+  -v ./scripts/:$HOME_DIR/scripts/:Z,ro \
+  -v ./docker/config/ssh_config:$HOME_DIR/.ssh/config:Z,ro \
+  -v ./docker/keys/ansible:$HOME_DIR/.ssh/id_rsa:Z,ro \
   --net $NETWORK_NAME \
   --network-alias ansible \
   playbook_ansible \
