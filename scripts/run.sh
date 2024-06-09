82305dfd418f535f399702ea3b745541dd756c05
Andrii <6715948+Insidexa@users.noreply.github.com>
2024-06-09 15:25:07 +0300

    use volumes intead of copy ( compatibility with dev exp ) (#5)
    * use volumes intead of copy ( compatibility with dev exp )
    
    * fix: add HOME_DIR env
    
    * chore: add version for dest base image
    
    * add ansible lint & fix perm
    
    * fix: work dir for ansible lint
    
    * fix: work dir for ansible lint & gen ssh keys
    
    * fix: keep kp dir
    
    * debug
    
    * chore: remove unneeded
    
    * refactor: remove unused, add uid to scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,7 @@
 NETWORK_NAME="ansible_bridge"
 PROJECT_DIR=$(pwd)
 USER_CNT_NAME="ansible"
+HOME_DIR="/home/$USER_CNT_NAME"
 
 # Check if the network exists
 if ! podman network inspect $NETWORK_NAME &>/dev/null; then
@@ -10,12 +11,15 @@ if ! podman network inspect $NETWORK_NAME &>/dev/null; then
     podman network create $NETWORK_NAME
 fi
 
-podman build -t playbook_playground --build-arg USER=$USER_CNT_NAME -f docker/Dockerfile.dest .
-podman build -t playbook_ansible --build-arg USER=$USER_CNT_NAME -f docker/Dockerfile.ansible .
+podman build -t playbook_playground --build-arg UID=$UID --build-arg USER=$USER_CNT_NAME -f docker/Dockerfile.dest .
+podman build -t playbook_ansible --build-arg UID=$UID --build-arg USER=$USER_CNT_NAME -f docker/Dockerfile.ansible .
+
+ssh-keygen -t rsa -b 4096 -C "ansible" -f ./docker/keys/id_rsa -N ""  <<< 'y'
 
 podman run \
   --detach \
   --name=playground \
+  -v ./docker/keys/id_rsa.pub:$HOME_DIR/.ssh/authorized_keys:Z,ro \
   --net $NETWORK_NAME \
   --network-alias playground \
   -u root \
@@ -26,6 +30,10 @@ podman run \
   --name=ansible \
   --userns=keep-id \
   --requires=playground \
+  -v ./playbook/:$HOME_DIR/playbook/:Z,ro \
+  -v ./scripts/:$HOME_DIR/scripts/:Z,ro \
+  -v ./docker/config/ssh_client:$HOME_DIR/.ssh/config:Z,ro \
+  -v ./docker/keys/id_rsa:$HOME_DIR/.ssh/id_rsa:Z,ro \
   --net $NETWORK_NAME \
   --network-alias ansible \
   playbook_ansible \
