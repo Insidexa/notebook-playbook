6a108aea6dbde89d70a50702eb865d5dcf9bb84e
Andrii Yakovenko <i.am.yashka@gmail.com>
2024-06-09 15:17:50 +0300

    refactor: remove unused, add uid to scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,4 @@
 source /etc/profile
 cd ./playbook
 ansible-galaxy install -r requirements.yml 
-ssh-keyscan -H $(sed -n 2p hosts/docker) >> ~/.ssh/known_hosts
 ansible-playbook -vv install.yml -i hosts/docker --tags "install"
--- scripts/run.sh
+++ scripts/run.sh
@@ -11,8 +11,8 @@ if ! podman network inspect $NETWORK_NAME &>/dev/null; then
     podman network create $NETWORK_NAME
 fi
 
-podman build -t playbook_playground --build-arg USER=$USER_CNT_NAME -f docker/Dockerfile.dest .
-podman build -t playbook_ansible --build-arg USER=$USER_CNT_NAME -f docker/Dockerfile.ansible .
+podman build -t playbook_playground --build-arg UID=$UID --build-arg USER=$USER_CNT_NAME -f docker/Dockerfile.dest .
+podman build -t playbook_ansible --build-arg UID=$UID --build-arg USER=$USER_CNT_NAME -f docker/Dockerfile.ansible .
 
 ssh-keygen -t rsa -b 4096 -C "ansible" -f ./docker/keys/id_rsa -N ""  <<< 'y'
 
@@ -32,6 +32,7 @@ podman run \
   --requires=playground \
   -v ./playbook/:$HOME_DIR/playbook/:Z,ro \
   -v ./scripts/:$HOME_DIR/scripts/:Z,ro \
+  -v ./docker/config/ssh_client:$HOME_DIR/.ssh/config:Z,ro \
   -v ./docker/keys/id_rsa:$HOME_DIR/.ssh/id_rsa:Z,ro \
   --net $NETWORK_NAME \
   --network-alias ansible \
